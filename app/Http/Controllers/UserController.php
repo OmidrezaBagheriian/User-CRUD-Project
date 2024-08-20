490c0173097589d58808820c94e892500f869046
Omidreza Bagheriyan <OmidrezaBagheriyan@gmail.com>
2024-08-21 03:09:58 +0330

    OmidrezaBagheriian-UserList / Update User in User List

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,55 +2,81 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\User; //import this
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash; //import this for password hashing
 
 class UserController extends Controller
 {
-    public function loadAllUsers()
-    {
+    //here create all crud logic
+    public function loadAllUsers(){
         $all_users = User::all();
         return view('users',compact('all_users'));
     }
 
-    public function loadAddUser()
-    {
+    public function loadAddUserForm(){
         return view('add-user');
     }
 
-    public function AddUser(Request $request)
-    {
+    public function AddUser(Request $request){
+        // perform form validation here
         $request->validate([
             'full_name' => 'required|string',
             'email' => 'required|email|unique:users',
             'phone_number' => 'required',
-            'password' => 'required|confirmed|min:8|max:64',
+            'password' => 'required|confirmed|min:4|max:8',
         ]);
-
         try {
+            // register user here
             $new_user = new User;
-            $new_user->name = $request->input('full_name');
-            $new_user->email = $request->input('email');
-            $new_user->phone_number = $request->input('phone_number');
-            $new_user->password = Hash::make($request->input('password'));
+            $new_user->name = $request->full_name;
+            $new_user->email = $request->email;
+            $new_user->phone_number = $request->phone_number;
+            $new_user->password = Hash::make($request->password);
             $new_user->save();
-            return redirect('/users')->with('success', 'User added successfully');
-        }catch (\exception $exception){
-            return redirect('/add/user')->with('fail', $exception->getMessage());
+
+            return redirect('/users')->with('success','User Added Successfully');
+        } catch (\Exception $e) {
+            return redirect('/add/user')->with('fail',$e->getMessage());
+        }
+
+
+    }
+
+    public function EditUser(Request $request){
+        // perform form validation here
+        $request->validate([
+            'full_name' => 'required|string',
+            'email' => 'required|email',
+            'phone_number' => 'required',
+        ]);
+        try {
+            // update user here
+            $update_user = User::where('id',$request->user_id)->update([
+                'name' => $request->full_name,
+                'email' => $request->email,
+                'phone_number' => $request->phone_number,
+            ]);
+
+            return redirect('/users')->with('success','User Updated Successfully');
+        } catch (\Exception $e) {
+            return redirect('/edit/user')->with('fail',$e->getMessage());
         }
     }
 
-    public function deleteUser($id)
-    {
+    public function loadEditForm($id){
+        $user = User::find($id);
+
+        return view('edit-user',compact('user'));
+    }
+
+    public function deleteUser($id){
         try {
             User::where('id',$id)->delete();
-            return redirect('/users')->with('success', 'User deleted successfully');
-        } catch (\exception $exception){
-            return redirect('/users')->with('fail', $exception->getMessage());
-        }
-    }
-    public function loadEditUser($id){
+            return redirect('/users')->with('success','User Deleted successfully!');
+        } catch (\Exception $e) {
+            return redirect('/users')->with('fail',$e->getMessage());
 
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController; //import this
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/users',[UserController::class,'loadAllUsers']);
-Route::get('add/user',[UserController::class,'loadAddUser']);
-Route::post('add/user',[UserController::class,'AddUser'])->name('AddUser');
-Route::get('edit/{id}',[UserController::class,'loadEditForm']);
-Route::get('delete/{id}',[UserController::class,'deleteUser']);
+Route::get('/add/user',[UserController::class,'loadAddUserForm']);
+
+Route::post('/add/user',[UserController::class,'AddUser'])->name('AddUser');
+
+Route::get('/edit/{id}',[UserController::class,'loadEditForm']);
+Route::get('/delete/{id}',[UserController::class,'deleteUser']);
+
+Route::post('/edit/user',[UserController::class,'EditUser'])->name('EditUser');
